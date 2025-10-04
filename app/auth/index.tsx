577a291a15d35febd15601041f702bdd4b0a4cad
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-10-04 14:29:28 -0300

    estrutura  e estilização inicial tela de autenticação

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Text, View, Button, Alert } from "react-native";
+import { Text, View, TouchableOpacity, SafeAreaView, Alert } from "react-native"; // Adicionei Alert
 import { useRouter } from "expo-router";
 import CustomInput from "../../assets/components/customInput";
-import { loginUsuario } from "../../services/api";
+import { globalStyles } from "../../assets/styles/globalStyles";
+import { loginUsuario } from // add caminho API
 
 export default function Index() {
   const router = useRouter()
@@ -20,14 +21,58 @@ export default function Index() {
   };
 
   return (
-    <View>
-      <Text>Tela Inicial (Login)</Text>
 
-      <CustomInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <CustomInput placeholder="Senha" value={senha} onChangeText={setSenha} secureTextEntry />
+  <SafeAreaView style={styles.container}> 
+      <View style={styles.content}> 
+        <Text style={styles.title}>Tela Inicial (Login)</Text>
+
+        <CustomInput placeholder="Email" value={email} onChangeText={setEmail} />
+        <CustomInput placeholder="Senha" value={senha} onChangeText={setSenha} secureTextEntry />
       
-      <Button title="Login" onPress={handleLogin} />
-      <Button title="Cadastrar" onPress={() => router.push("/auth/signUp")} />  
-    </View>
+       <TouchableOpacity style={globalStyles.button} onPress={handleLogin} >
+          <Text style={globalStyles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={globalStyles.button} onPress={() => router.push("/auth/signUp")} >
+          <Text style={globalStyles.buttonText}>Cadastrar</Text>
+        </TouchableOpacity> 
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: "grey"
+  },
+
+  content: {
+    marginHorizontal: 24,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#070707ff',
+    textAlign: 'center',
+     marginBottom: 14, // Espaço abaixo do título
+  },
+
+  // Botão
+
+  button: {
+    backgroundColor: '#007bff', // Azul
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center', // Centraliza o texto
+    marginTop: 16, // Espaço acima do botão
+  }, 
+
+  buttonText: {
+    color: '#fff', // Texto branco
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
